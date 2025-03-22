9d353f4d671a86cb0ee7e0e2876d2da4353bfc58
Ron Negrov <m4gapower@gmail.com>
2025-03-22 22:06:52 +0200

    Added git connect function

--- fertility.sh
+++ fertility.sh
@@ -23,6 +23,7 @@ package_list=()
     project_path=$(realpath -m "$(eval echo "$raw_path")")
     cd "$project_path" || { echo "Path does not exist"; exit 1; }
     read -p "Initialize Git repository? [Y/n] " git_enabled
+    read -p "Initialize a virtual environment? [Y/n] " venv_enable
     full_path="$project_path/$project_name"
     if [[ ! -e $full_path ]]; then
     mkdir -p "$full_path"
@@ -33,6 +34,15 @@ package_list=()
     if [[ "$git_enabled" == "Y" || "$git_enabled" == "y" ]]; then
         git init >/dev/null 2>&1
         echo "Git repository initialized."
+     read -p "Would you like to connect your repository to a remote github repository? [Y/n] " remote_git_enabled
+         if [[ "$remote_git_enabled" == "Y" || "$remote_git_enabled" == "y" ]]; then
+            read -p "What is your github username? " username
+            read -p "What is the name of the repository that you want to be connected to " repository
+            git_connect "$username" "$repository" "$full_path"
+
+         fi
+
+
     fi
     echo "Project $project_name created at $project_path"
 
@@ -40,7 +50,9 @@ package_list=()
  ask_user_packages package_list
  distro_check_and_install "${package_list[@]}"
 
+if [[ "$venv_enabled" == "Y" || "$venv_enabled" == "y" ]]; then
  venv_init "$full_path"
+ fi
 
 }
 
